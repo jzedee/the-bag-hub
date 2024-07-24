d9f8d1c883c8c1c39cc35f27bc2d134b562c221f
Sarvesh JD <sdahre3@gmail.com>
2024-07-25 04:31:18 +0530

    Initial commit

---
+++ App_Payment/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from App_Payment import views
+
+app_name = 'App_Payment'
+
+urlpatterns = [
+    path('checkout/', views.checkout, name='checkout'),
+    path('pay/', views.payment, name='payment'),
+    path('status/', views.complete, name='complete'),
+    path('purchase/<val_id>/<tran_id>/', views.purchased, name='purchase'),
+    path('order/', views.order_view, name='orders'),
+    path('complete_payment/', views.complete_payment, name='complete_payment'),
+]
